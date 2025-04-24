81a132fd09b6294ad5384290f42393c9b32316f2
Vansh Vadhwa <vvad0647@gmail.com>
2025-04-24 10:24:39 +0000

    docker update push

---
+++ react-app/kuberg12/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>This is Docker React Minikube</h1>
+      <h3>created by Vansh Vadhwa</h3>
+    </div>
+  )
+}
+
+export default App
